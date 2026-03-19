7be4bfd026c4e0ca248bc77a1c6de3c61d6b1984
anwjrdid <yangchaehyeon67@gmail.com>
2026-03-19 10:30:00 +0900

    과제1 코딩과테스트완료 중앙의 output_window는 전송된 데이터를 목록화하여 보여주며, 하단의 input_window는 텍스트를 입력하는 핵심 공간입니다. Btn_Forwarding을 클릭하면 작성한 내용이 리스트에 즉시 추가됨과 동시에 입력창은 깨끗하게 비워져 다음 작업을 연속해서 처리하기에 매우 효율적인 구조입니다. 사용자는 이 과정을 통해 입력과 결과 확인을 직관적으로 수행 할 수 있습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,25 +29,60 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            output_window = new ListBox();
+            input_window = new TextBox();
+            Btn_Forwarding = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("문체부 훈민정음체", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = SystemColors.HotTrack;
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(307, 45);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Echo Messenger";
+            // 
+            // output_window
+            // 
+            output_window.FormattingEnabled = true;
+            output_window.Location = new Point(12, 66);
+            output_window.Name = "output_window";
+            output_window.Size = new Size(546, 264);
+            output_window.TabIndex = 1;
+            // 
+            // input_window
+            // 
+            input_window.Location = new Point(12, 350);
+            input_window.Name = "input_window";
+            input_window.Size = new Size(439, 27);
+            input_window.TabIndex = 2;
+            // 
+            // Btn_Forwarding
+            // 
+            Btn_Forwarding.BackColor = Color.LawnGreen;
+            Btn_Forwarding.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            Btn_Forwarding.Location = new Point(457, 338);
+            Btn_Forwarding.Name = "Btn_Forwarding";
+            Btn_Forwarding.Size = new Size(101, 50);
+            Btn_Forwarding.TabIndex = 3;
+            Btn_Forwarding.Text = "전송";
+            Btn_Forwarding.UseVisualStyleBackColor = false;
+            Btn_Forwarding.Click += Btn_Forwarding_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(591, 409);
+            Controls.Add(Btn_Forwarding);
+            Controls.Add(input_window);
+            Controls.Add(output_window);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Echo Messenger";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +90,8 @@
         #endregion
 
         private Label label1;
+        private ListBox output_window;
+        private TextBox input_window;
+        private Button Btn_Forwarding;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,14 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void Btn_Forwarding_Click(object sender, EventArgs e)
+        {
+            string typed_msg = input_window.Text; // 입력창에 입력된 텍스트를 가져와서 typed_msg 변수에 저장
+
+            output_window.Items.Add(typed_msg); // output_window 리스트박스에 typed_msg를 추가하여 출력창에 메시지를 표시
+
+            input_window.Clear(); // 입력창을 비워서 다음 메시지를 입력할 수 있도록 준비
+        }
     }
 }
